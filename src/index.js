9a6d963f54bc4eb896f33131a37dbe9b285e0848
unyonnyon <6unyon21sdvx@gmail.com>
2020-12-13 19:26:56 +0900

    gettint-started-before-webpack

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function component() {
+  const element = document.createElement("div");
+  const array = ["Hello", "Webpack"];
+  element.innerHTML = _.join(array, "xxx");
+  return element;
+}
+
+document.body.appendChild(component());
+document.body.appendChild(component());
