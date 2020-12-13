a719e5fe3957f9ed0740b3757d80341993c0ddee
unyonnyon <6unyon21sdvx@gmail.com>
2020-12-13 20:07:48 +0900

    intorduce webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import _ from "lodash";
 function component() {
   const element = document.createElement("div");
   const array = ["Hello", "Webpack"];
-  element.innerHTML = _.join(array, "xxx");
+  element.innerHTML = _.join(array, "xxxxx");
   return element;
 }
 
