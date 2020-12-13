3378e27a9b72df941f8b50e545d481b15f5cdd52
unyonnyon <6unyon21sdvx@gmail.com>
2020-12-14 02:44:38 +0900

    append url-loader and file-loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from "lodash";
 import Lion, { byDouble, TWO } from "./utilities";
 import "./style.css";
+import logo from "./king035.jpg";
 
 function component() {
   const element = document.createElement("div");
@@ -13,3 +14,7 @@ console.log(byDouble(TWO));
 document.body.appendChild(component());
 document.body.appendChild(component());
 document.body.classList.add("haikei");
+
+const image = new Image();
+image.src = logo;
+document.body.appendChild(image);
