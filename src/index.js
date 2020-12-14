ac81eb3e8f9091fbd066e5a64022d160bffa854f
unyonnyon <6unyon21sdvx@gmail.com>
2020-12-15 01:36:01 +0900

    setup react project and babel

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
-import _ from "lodash";
-import Lion, { byDouble, TWO } from "./utilities";
-import "./style.css";
+import React from "react";
+import ReactDOM from "react-dom";
 import "./style.scss";
-import logo from "./king035.jpg";
 
-function component() {
-  const element = document.createElement("div");
-  const array = ["Hello", "Webpack"];
-  element.innerHTML = _.join(array, Lion.say());
-  return element;
-}
-
-console.log(byDouble(TWO));
-document.body.appendChild(component());
-document.body.appendChild(component());
-document.body.classList.add("haikei");
-
-const image = new Image();
-image.src = logo;
-document.body.appendChild(image);
+ReactDOM.render(<div>Hello, React!</div>, document.getElementById("root"));
